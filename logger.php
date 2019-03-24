82f308618477603e12472a7ad8d0986dffd322a8
Kowalsky15 <42894466+Kowalsky15@users.noreply.github.com>
2019-03-24 18:44:03 -0400

    Update logger.php

--- logger.php
+++ logger.php
@@ -6,7 +6,7 @@ require_once('rabbitMQLib.inc');
 
 function writeErrors($date, $errorMSG)
 {
-    file_put_contents('error.log', "[".$date."]".$errorMSG.PHP_EOL, FILE_APPEND);
+    file_put_contents('/home/nick/Desktop/error.log', "[".$date."]".$errorMSG.PHP_EOL, FILE_APPEND);
 }
 
 function requestProcessor($request)
@@ -17,7 +17,7 @@ function requestProcessor($request)
     return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
 
-$server = new rabbitMQServer("errorServer.ini","errorServer");
+$server = new rabbitMQServer("logger.ini","testServer");
 echo "Error Server has started.".PHP_EOL;
 $server->process_requests('requestProcessor');
 echo "Error Server has stopped.".PHP_EOL;
