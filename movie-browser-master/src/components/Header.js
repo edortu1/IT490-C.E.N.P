9e512736376e39373315a22f375ed5453556b4b2
Kowalsky15 <42894466+Kowalsky15@users.noreply.github.com>
2019-03-27 15:47:27 -0400

    Add files via upload

--- movie-browser-master/src/components/Header.js
+++ movie-browser-master/src/components/Header.js
@@ -11,6 +11,8 @@ class Header extends Component {
             `<header class="header">
                 <div class="header__wrapper wrapper wrapper--constrained">
                     <a href="/" class="header__title"><span class="header__logo">${iconMovie()}</span> <span>Kaplunk</span></a>
+<br>
+<a href="logout.php">Log Out</a>
                 </div>
             </header>`
         );
