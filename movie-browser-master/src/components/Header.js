13c5ecc3cd34d4fc9657be1e698a61b14ad420eb
Kowalsky15 <42894466+Kowalsky15@users.noreply.github.com>
2019-03-26 23:17:39 -0400

    Update Header.js

--- movie-browser-master/src/components/Header.js
+++ movie-browser-master/src/components/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
         return (
             `<header class="header">
                 <div class="header__wrapper wrapper wrapper--constrained">
-                    <a href="/" class="header__title"><span class="header__logo">${iconMovie()}</span> <span>TMDB Movie Browser</span></a>
+                    <a href="/" class="header__title"><span class="header__logo">${iconMovie()}</span> <span>Kaplunk</span></a>
                 </div>
             </header>`
         );
